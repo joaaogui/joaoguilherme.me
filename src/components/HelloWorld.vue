d3d2cf4f61705721e76ae004d0e62191f8007581
João Guilherme <joaaogui@gmail.com>
2023-02-25 23:44:43 -0300

    .

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
-<script setup lang="ts">
+<script setup>
 import { ref } from "vue";
 
 const count = ref(0);
-function goTo(url: string) {
+function goTo(url) {
   window.location.href = url;
 }
 </script>
@@ -10,43 +10,23 @@ function goTo(url: string) {
 <template>
   <div id="app">
     <div>
-      dsdfdsfsdfds
-      <div>
-        <div class="greeting">
-          <h1>Oi!</h1>
-        </div>
-        <div class="main">
-          <v-col cols="12">
-            <p>My name is João Guilherme</p>
-          </v-col>
-          <v-col cols="12">
-            <p>My email is me@joaoguilherme.me</p>
-          </v-col>
-          <v-col cols="12">
-            <p>I'm a software engineer</p>
-          </v-col>
-        </div>
+      <h1>Oi!</h1>
+      <div class="main">
+        <p>My name is João Guilherme</p>
+        <p>My email is me@joaoguilherme.me</p>
+        <p>I'm a software engineer</p>
+      </div>
 
-        <div class="icons">
-          <div cols="6" style="text-align: end">
-            <div>
-              <div
-                size="80"
-                class="mr-2"
-                @click="goTo('https://www.linkedin.com/in/joaaogui/')"
-              >
-                mdi-linkedin
-              </div>
-            </div>
-          </div>
-          <div cols="6" style="text-align: start">
-            <div>
-              <div size="80" @click="goTo('https://github.com/joaaogui/')">
-                mdi-github
-              </div>
-            </div>
+      <div class="icons">
+        <div>
+          <div @click="goTo('https://www.linkedin.com/in/joaaogui/')">
+            mdi-linkedin
           </div>
         </div>
+        <div @click="goTo('https://github.com/joaaogui/')">mdi-github</div>
+        <div @click="goTo('/youtube')">Youtube</div>
+        <div @click="goTo('/spotify')">Spotify</div>
+        <div @click="goTo('/imdb')">Imdb</div>
       </div>
     </div>
   </div>
